4f19acbf04e1e9031556ac3030babce23a1931f4
Ivan <ivan.korshunov.1997@gmail.com>
2023-06-17 22:57:49 +0300

    create TaskType and Task models

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Task(models.Model):
+    PRIORITY_CHOICES = (
+        ('normal', 'Normal'),
+        ('urgent', 'Urgent'),
+        ('critical', 'Critical'),
+    )
+
+    name = models.CharField(max_length=255, null=False)
+    description = models.TextField()
+    deadline = models.DateField()
+    is_completed = models.BooleanField()
+    priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES)
+    task_type = models.ForeignKey(TaskType, on_delete=models.SET_NULL)
