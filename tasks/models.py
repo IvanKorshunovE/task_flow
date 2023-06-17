1907bf259f1611e1240c399b17bae4a5119c82ee
Ivan <ivan.korshunov.1997@gmail.com>
2023-06-17 23:11:05 +0300

    add class Meta to  TaskType and  Task models, create Position and Worker models

--- tasks/models.py
+++ tasks/models.py
@@ -1,8 +1,28 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = "Position"
+        verbose_name_plural = "Positions"
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(Position, on_delete=models.SET_NULL)
 
 
 class TaskType(models.Model):
     name = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = "Task type"
+        verbose_name_plural = "Task types"
+
+    def __str__(self):
+        return f"{self.name}"
 
 
 class Task(models.Model):
@@ -18,3 +38,11 @@ class Task(models.Model):
     is_completed = models.BooleanField()
     priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES)
     task_type = models.ForeignKey(TaskType, on_delete=models.SET_NULL)
+
+    class Meta:
+        ordering = ["-priority"]
+        verbose_name = "Task"
+        verbose_name_plural = "Tasks"
+
+    def __str__(self):
+        return f"{self.name}"
